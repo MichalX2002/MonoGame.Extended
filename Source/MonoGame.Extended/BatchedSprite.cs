660cfe5d0b6f496c1b2698039694892b84aaa741
Michal Piatkowski <michal@kampinas.se>
2018-06-12 19:30:41 +0200

    Changed GlyphEnumerator to get passed by readonly ref

--- Source/MonoGame.Extended/BatchedSprite.cs
+++ Source/MonoGame.Extended/BatchedSprite.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.TextureAtlases;
-using System.Runtime.CompilerServices;
 
 namespace MonoGame.Extended
 {
