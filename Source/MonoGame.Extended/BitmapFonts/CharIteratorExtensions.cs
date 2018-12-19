0b6a66b2b4f238e7c6dc76280ae70bd3953a3f8b
Michal Piatkowski <michal@kampinas.se>
2018-12-19 22:11:51 +0100

    Added ToIterator() extensions for 'string' and 'StringBuilder'

---
+++ Source/MonoGame.Extended/BitmapFonts/CharIteratorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace MonoGame.Extended.BitmapFonts
+{
+    public static class CharIteratorExtensions
+    {
+        public static ICharIterator ToIterator(this StringBuilder value, int offset, int count)
+        {
+            return CharIteratorPool.Rent(value, offset, count);
+        }
+
+        public static ICharIterator ToIterator(this StringBuilder value)
+        {
+            return ToIterator(value, 0, value.Length);
+        }
+
+        public static ICharIterator ToIterator(this string value, int offset, int count)
+        {
+            return CharIteratorPool.Rent(value, offset, count);
+        }
+
+        public static ICharIterator ToIterator(this string value)
+        {
+            return ToIterator(value, 0, value.Length);
+        }
+    }
+}
